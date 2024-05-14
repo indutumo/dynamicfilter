92cd33f23e8359e1ad23473e9777a547530c9cc9
root <root@DESKTOP-FDL90FV.localdomain>
2024-05-14 09:33:38 +0300

    pdf export

--- population/urls.py
+++ population/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('population_list', views.population_list, name='population_list'),
     path('population_filter', views.population_filter, name='population_filter'),
+    path('population_list_pdf', views.population_list_pdf, name='population_list_pdf'),
     ]
--- population/views.py
+++ population/views.py
@@ -3,6 +3,12 @@ import requests
 from django.shortcuts import render, redirect
 from .models import  Population
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.db.models import Sum, Avg, F, Q
+from datetime import date
+from django.contrib.auth.decorators import login_required
     
 
 def is_valid_queryparam(param):
@@ -12,10 +18,13 @@ def is_valid_queryparam(param):
 def population_list(request):
     login_url = '/'
     qs = Population.objects.order_by('-year')
-    
 
     name = request.GET.get('name')
     year = request.GET.get('year')
+
+    request.session['name'] = name
+    request.session['year'] = year
+    print(request.session['name'])
 
     if is_valid_queryparam(name):
         qs = qs.filter(name__icontains=name)
@@ -35,9 +44,7 @@ def population_list(request):
 
     year_list = Population.objects.values_list('year', flat=True).order_by('year').distinct()
     country_list = Population.objects.values_list('name', flat=True).order_by('name').distinct()
-
-    print(year_list)
-
+    
     context = {
         'population_list': qs,
         'year_list':year_list,
@@ -51,10 +58,12 @@ def population_list(request):
 def population_filter(request):
     login_url = '/'
     qs = Population.objects.order_by('-year')
-    
 
     name = request.GET.get('name')
     year = request.GET.get('year')
+
+    request.session['name'] = name
+    request.session['year'] = year
 
     if is_valid_queryparam(name):
         qs = qs.filter(name__icontains=name)
@@ -78,3 +87,46 @@ def population_filter(request):
         'name':name,
     }
     return render(request, "population_filter.html", context)
+
+
+def population_list_pdf(request):
+    login_url = '/'
+    template_path = 'population_list_pdf.html'
+    qs = Population.objects.order_by('-year')
+
+    if 'name' in request.session:
+        name = request.session['name']
+    else:
+        name = None
+
+    if 'year' in request.session:
+        year = request.session['year']
+    else:
+        year = None
+
+    if is_valid_queryparam(name):
+        qs = qs.filter(name__icontains=name)
+
+    if is_valid_queryparam(year):
+        qs = qs.filter(year__icontains=year)
+
+    today = date.today()
+    population = qs.aggregate(Sum('value'))['value__sum']
+    context = {'population_list':qs,'population':population,'today':today}
+    response = HttpResponse(content_type='application/pdf')
+    if name is None:
+        response['Content-Disposition'] = 'filename="World Population.pdf"'
+    elif name:
+        response['Content-Disposition'] = 'filename='+ str(name.capitalize()) + " " + str("Population.pdf")
+    elif year:
+        response['Content-Disposition'] = 'filename='+ str(year) + " " + str("World Population.pdf")
+
+    # Find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # Create a PDF
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
